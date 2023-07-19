db376c41905f22155065ddad7b1e7e76190f2e66
RabihAhmadieh <rabih.ahm77@gmail.com>
2023-07-19 22:48:39 +0300

    Add files via upload

---
+++ PDF_Text.py
@@ -0,0 +1,12 @@
+import fitz
+
+pdf = fitz.Document("Test PDF file.pdf")
+page = pdf[0]
+text = page.get_textpage().extractText().encode("utf-8")
+
+out = open("output.txt", "wb") # create a text output
+out.write(text) # write text of page
+out.write(bytes((12,))) # write page delimiter (form feed 0x0C)
+
+out.close()
+print(text)
